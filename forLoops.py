3b61682d35dbe1ce739aef9340372bb5cfd3fa6f
Srinjoycode <srinjoybhuiya@gmail.com>
2020-05-24 20:56:15 +0530

    first merge on new branch

--- forLoops.py
+++ forLoops.py
@@ -1,3 +1,8 @@
 for i in range(0, 11, 2):
     int mul = 2*i
     print(f"{i}*2={mu;}")
+print("Done with for loop")
+print("wait for a while loop")
+while i < 15:
+    i += 1
+    print(i)
--- sampleGit.py
+++ sampleGit.py
@@ -1,2 +1,3 @@
 i = "hello World"
 print(i)
+print("new branch 1 ")
